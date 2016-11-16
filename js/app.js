d427b681cc11c907e13c35e2456b3dd17a4c064c
KielRodriguez <kr.rdz.20@gmail.com>
2016-11-16 01:22:05 -0600

    initial commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('#banner').css( { "height" : $(window).height() + "px" });
+
+  var flag = false;
+  var scroll;
+
+  $(window).scroll(function(){
+    scroll = $(window).scrollTop();
+
+    if( scroll > 200 ){
+      if( !flag ){
+        $("#logo").css({"margin-top":"-5px", "width":"50px", "height": "50px"});
+        flag = true;
+      }
+    } else {
+      if( flag ){
+        $("#logo").css({"margin-top":"150px", "width":"250px", "height": "250px"});
+        flag = false;
+      }
+    }
+  });
+});
